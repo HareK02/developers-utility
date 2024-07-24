72185b34ca652377aa70ec96bf0fe0f3b332d715
Hare <hare.02.k@gmail.com>
2024-07-24 18:27:40 +0000

    feat: Add logic to init entry config

--- src/main/kotlin/net/hareworks/developutil/App.kt
+++ src/main/kotlin/net/hareworks/developutil/App.kt
@@ -6,21 +6,30 @@ import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 
 public class App : JavaPlugin() {
-    companion object {
-        lateinit var instance: App
-            private set
-    }
-    override fun onEnable() {
-        instance = this
-        saveDefaultConfig()
-        initEntries()
-        Key.initialize()
-    }
-}
+  companion object {
+    lateinit var instance: App
+      private set
+  }
+  override fun onEnable() {
+    instance = this
+    initEntries()
+    Key.initialize()
+  }
 
-fun initEntries() {
-    val file = File(App.instance.getDataFolder(), "entries.yml")
-    val config = YamlConfiguration.loadConfiguration(file)
-    val entries = config.getConfigurationSection("entries")
-    entries!!.getKeys(false).forEach { key -> Entry.addEntry(key, entries.getString(key)!!) }
+  fun initEntries() {
+    val file = File(getDataFolder(), "entries.yml")
+    if (!file.exists()) {
+      file.getParentFile().mkdirs()
+      saveResource("entries.yml", false)
+    }
+    val config = YamlConfiguration()
+    try {
+      config.load(file)
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+    config.getMapList("entries")?.forEach { entry ->
+      App.instance.getLogger().info("Loading entry: $entry")
+    }
+  }
 }
