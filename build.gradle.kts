fca03a9971be6b967c088bb776921004c568043d
Hare <hare.02.k@gmail.com>
2024-07-24 17:33:22 +0000

    chore: Update build.gradle.kts for PaperPluginDescription

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import net.minecrell.pluginyml.paper.PaperPluginDescription
 
 group = "net.hareworks"
 version = "1.0"
 
 plugins {
     kotlin("jvm") version "1.9.23"
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 repositories {
@@ -15,7 +16,7 @@ repositories {
 }
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
@@ -27,24 +28,15 @@ tasks {
     }
 }
 
-bukkit {
+paper {
     main = "net.hareworks.developutil.App"
+    apiVersion = "1.20.6"
     name = "DevelopersUtility"
     description = ""
     version = getVersion().toString()
-    apiVersion = "1.20.6"
-    authors =
-            listOf(
+    authors = listOf(
                 "Hare-K02",
             )
-    commands {
-        register("devu") {
-            description = "develop-util command"
-            usage = "/devu"
-            permission = "devu.command"
-            permissionMessage = "You don't have permission to use this command."
-        }
-    }
     permissions {
         register("devu.command") {
             description = "Allows the player to use /devu command"
